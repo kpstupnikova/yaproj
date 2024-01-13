a5fc0dc257478399cffa19d7b06e5e9f82832ffc
Kseniia <kp.stupnikova@gmail.com>
2024-01-13 14:26:02 +0500

    Добавление шариков которые отталкиваются друг от друга и от стенок, добавление винальное заставки проигрыша если шарик коснется до персонажа, добавление записи времени в txt файл и лучшего результата времени

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-# Открываем файл с библиотеками
-with open('requirements.txt', 'r') as file:
-    # Читаем содержимое файла
-    libraries = file.readlines()
-    # Убираем символ перевода строки
-    libraries = [lib.strip() for lib in libraries]
+import os
+import random
+import sys
 
-# Импортируем библиотеки
-for lib in libraries:
-    try:
-        exec(f'import {lib}')
-    except ImportError:
-        print(f'Не удалось импортировать библиотеку: {lib}')
+import pygame
 
-FPS = 20
+# Инициализация основных объектов pygame
 pygame.init()
+FPS = 20
 size = width, height = 700, 500
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Сокобан")
 
+# Глобальные переменные
+frame_index = 0
+boxes_positions = {}
+tile_width = tile_height = 50
+# основной персонаж
+player = None
+# Инициализация групп спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+ball_group = pygame.sprite.Group()  # Группа для шариков
+bg = pygame.sprite.Group()
+pygame.init()
 # Загрузка изображений для анимации
 image1 = pygame.image.load('image1.png')
 image2 = pygame.image.load('image2.png')
@@ -31,8 +39,7 @@ image7 = pygame.image.load('image7.png')
 image8 = pygame.image.load('image8.png')
 image9 = pygame.image.load('image9.png')
 image10 = pygame.image.load('image10.png')
-frame_index = 0
-boxes_positions = {}
+
 # Цикл обработки каждого изображения
 animation_images = []
 for image in [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10]:
@@ -40,41 +47,6 @@ for image in [image1, image2, image3, image4, image5, image6, image7, image8, im
     processed_image = image.convert_alpha()
     processed_image.set_colorkey((255, 255, 255))
     animation_images.append(processed_image)
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-def check_win_condition(level):
-    for y in range(level_y):
-        for x in range(level_x):
-            # Если на месте точки нет ящика, то условие победы не выполнено
-            if level[y][x] == 'T' and (x, y) not in boxes_positions:
-                return False  # Условие победы не выполнено
-    return True  # Все ящики на своих местах
-
-
-# функция для отображения финального экрана победы
-def show_victory_screen():
-    fon = pygame.transform.scale(load_image('pobeda.jpg'), (width, height))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 100)  # Устанавливаем размер шрифта
-    victory_text = font.render("Победа", True, pygame.Color('black'))  # Текст с черным цветом
-    text_rect = victory_text.get_rect(center=(width // 2, height // 2))  # Располагаем текст по центру экрана
-    screen.blit(victory_text, text_rect)  # Отображаем текст на экране
-    pygame.display.flip()  # Обновляем экран, чтобы отобразить финальную заставку
-    # Ожидание выхода после победы
-    while True:
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                terminate()
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:  # Выход по нажатию Escape
-                    terminate()
 
 
 def load_image(name, colorkey=None):
@@ -91,6 +63,139 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+tile_images = {
+    'wall': load_image('box.jpg'),
+    'empty': load_image('grass.jpg'),
+    'tochka': load_image('tochka.jpg'),
+    'yachik': load_image('yachik.jpg')
+}
+tochka = load_image('tochka.jpg')
+yachik = load_image('yachik.jpg')
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.animation_images = animation_images
+        self.image_index = 0
+        self.image = self.animation_images[self.image_index]
+        self.pos = pos_x, pos_y
+        self.rect = self.image.get_rect(
+            center=(tile_width * pos_x + tile_width // 2, tile_height * pos_y + tile_height // 2))
+
+    def update(self, *args):
+        # Обновление анимации
+        self.image = self.animation_images[self.image_index]
+        self.image_index = (self.image_index + 1) % len(animation_images)
+        # Перемещение персонажа
+        self.rect.topleft = self.pos[0] * tile_width, self.pos[1] * tile_height
+
+    def move(self, x, y):
+        self.pos = x, y
+        self.rect.center = tile_width * x + tile_width // 2, tile_height * y + tile_height // 2
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius):
+        super().__init__(all_sprites, bg)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius), pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("pink"),
+                           (radius, radius), radius)
+        x = random.randint(0, width)
+        y = random.randint(0, height)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        while len(pygame.sprite.spritecollide(self, all_sprites, False)) > 1:
+            x = random.randint(0, width)
+            y = random.randint(0, height)
+            self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(0, 5)
+        self.vy = random.randrange(0, 5)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
+        ball_col = pygame.sprite.spritecollide(self, bg, False)
+        if len(ball_col) > 1:
+            for b in ball_col:
+                b.vx = -b.vx
+                b.vy = -b.vy
+
+
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
+for i in range(4):
+    ball = Ball(20)
+    ball_group.add(ball)  # Добавляем созданный шар в ball_group
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def check_win_condition(level):
+    for y in range(level_y):
+        for x in range(level_x):
+            # Если на месте точки нет ящика, то условие победы не выполнено
+            if level[y][x] == 'T' and (x, y) not in boxes_positions:
+                return False  # Условие победы не выполнено
+    return True  # Все ящики на своих местах
+
+
+def write_time_to_file(seconds):
+    try:
+        with open("times.txt", "a", encoding='utf-8') as file:
+            # Записываем время, за которое игрок прошел игру, в файл с новой строки
+            file.write(f"{seconds}\n")
+    except IOError as e:
+        print(f"Ошибка записи файла: {e}")
+        sys.exit()
+
+
+# функция для отображения финального экрана победы
+def show_victory_screen(seconds):
+    write_time_to_file(seconds)  # Записываем время прохождения в файл
+
+    fon = pygame.transform.scale(load_image('pobeda.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 100)
+    victory_text = font.render("Победа", True, pygame.Color('black'))
+    text_rect = victory_text.get_rect(center=(width // 2, height // 2))
+    screen.blit(victory_text, text_rect)
+    pygame.display.flip()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    terminate()
 
 
 def start_screen():
@@ -130,7 +235,46 @@ def start_screen():
     return choose_level()
 
 
+def show_loss_screen():
+    fon = pygame.transform.scale(load_image('proigrysh.jpg'), (width, height))  # Замените на ваше изображение проигрыша
+    screen.blit(fon, (0, 0))
+
+    font = pygame.font.Font(None, 100)  # Устанавливаем размер шрифта
+    loss_text = font.render("Проигрыш", True, pygame.Color('white'))  # Текст с черным цветом
+    text_rect = loss_text.get_rect(center=(width // 2, height // 2))  # Располагаем текст по центру экрана
+
+    screen.blit(loss_text, text_rect)  # Отображаем текст на экране
+
+    pygame.display.flip()  # Обновляем экран, чтобы отобразить финальную заставку проигрыша
+
+    # Ожидание выхода после проигрыша
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:  # Выход по нажатию Escape
+                    terminate()
+
+
+def get_best_time():
+    try:
+        with open("times.txt", "r", encoding='utf-8') as file:
+            times = file.readlines()
+            # Преобразуем строки в числа и фильтруем пустые строки
+            times = [int(time.strip()) for time in times if time.strip().isdigit()]
+            if times:  # Если в списке есть элементы
+                return min(times)
+            else:
+                return None
+    except IOError as e:
+        print(f"Ошибка чтения файла: {e}")
+        return None
+
+
 def choose_level():
+    best_time = get_best_time()  # Получаем лучшее время
+
     # Отображаем меню выбора уровня
     level_menu_text = ['Выберите уровень',
                        "1. Уровень 1",
@@ -153,6 +297,13 @@ def choose_level():
         intro_rect.x = 10
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
+
+    # Отображение лучшего времени
+
+    best_time_text = font.render(f"Лучший результат: {best_time}", True, pygame.Color('black'))
+    best_time_rect = best_time_text.get_rect()
+    best_time_rect.topright = (width - 10, 10)
+    screen.blit(best_time_text, best_time_rect)
 
     pygame.display.flip()
 
@@ -187,58 +338,11 @@ def load_level(filename):
     return [list(i) for i in list(map(lambda x: x.ljust(max_width, '.'), level_map))]
 
 
-tile_images = {
-    'wall': load_image('box.jpg'),
-    'empty': load_image('grass.jpg'),
-    'tochka': load_image('tochka.jpg'),
-    'yachik': load_image('yachik.jpg')
-}
-
-tochka = load_image('tochka.jpg')
-yachik = load_image('yachik.jpg')
-
-tile_width = tile_height = 50
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.animation_images = animation_images
-        self.image_index = 0
-        self.image = self.animation_images[self.image_index]
-        self.pos = pos_x, pos_y
-        self.rect = self.image.get_rect(
-            center=(tile_width * pos_x + tile_width // 2, tile_height * pos_y + tile_height // 2))
-
-    def update(self, *args):
-        # Обновление анимации
-        self.image = self.animation_images[self.image_index]
-        self.image_index = (self.image_index + 1) % len(animation_images)
-        # Перемещение персонажа
-        self.rect.topleft = self.pos[0] * tile_width, self.pos[1] * tile_height
-
-    def move(self, x, y):
-        self.pos = x, y
-        self.rect.center = tile_width * x + tile_width // 2, tile_height * y + tile_height // 2
-
-
-# основной персонаж
-player = None
-
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-
-
 def generate_level(level):
+    Border(5, 5, width - 5, 5)
+    Border(5, height - 5, width - 5, height - 5)
+    Border(5, 5, 5, height - 5)
+    Border(width - 5, 5, width - 5, height - 5)
     new_player, level_x, level_y = None, len(level[0]), len(level)
     for y in range(level_y):
         for x in range(level_x):
@@ -258,6 +362,7 @@ def generate_level(level):
                 Tile('empty', x, y)
                 new_player = Player(x, y)
                 level[y][x] = '.'
+
     return new_player, level_x, level_y
 
 
@@ -293,8 +398,9 @@ def can_move(whom, where, level):
 
             # Проверка условий победы
             if check_win_condition(map_level):
-                show_victory_screen()  # Отображаем финальный экран
-                running = False  # Завершаем игру
+                seconds = (pygame.time.get_ticks() - start_ticks) // 1000
+                show_victory_screen(seconds)  # Передаем время функции
+                running = False
 
         elif object_in_front == '.':
             whom.move(x + dx, y + dy)
@@ -363,17 +469,24 @@ while running:
     all_sprites.update()  # Обновляем все спрайты
     tiles_group.draw(screen)  # Отображаем только группу спрайтов с тайлами
     player_group.draw(screen)  # Отображаем только группу спрайтов с персонажем
+    ball_group.draw(screen)  # Рисуем шарики последними, чтобы они были наверху
+    ball_group.update()  # Обновляем шарики
+
+    # Проверка столкновения шарика и персонажа
+    if pygame.sprite.spritecollideany(player, ball_group):  # Проверяем столкновение персонажа с любым шаром
+        show_loss_screen()  # Показываем экран проигрыша вместо простого вывода сообщения
+        running = False  # Прекращаем игровой цикл
 
     font = pygame.font.Font(None, 30)
-    # Отображение таймера
-    seconds = (pygame.time.get_ticks() - start_ticks) // 1000  # Преобразование миллисекунд в секунды
-    timer_text = font.render(f"Время: {seconds}", True, pygame.Color('white'))
+
+    # Отображение таймера    seconds = (pygame.time.get_ticks() - start_ticks) // 1000  # Преобразование миллисекунд в секунды
+    timer_text = font.render(f"Время: {seconds}", True, pygame.Color('black'))
     timer_rect = timer_text.get_rect()
     timer_rect.topleft = (10, 10)
     screen.blit(timer_text, timer_rect)
 
     # Обновление счетчик шагов
-    movements_text = font.render(f"Количество нажатий: {movements_count}", True, pygame.Color('white'))
+    movements_text = font.render(f"Количество нажатий: {movements_count}", True, pygame.Color('black'))
     movements_rect = movements_text.get_rect()
     movements_rect.topleft = (10, 50)
     screen.blit(movements_text, movements_rect)
